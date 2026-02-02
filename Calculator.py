871972f7aa390070df00b7a5add85841c16a5f35
Not-So-Pro-Banda <saadbinishtiaq9@gmail.com>
2026-02-02 12:56:51 +0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+a = eval(input("Enter a number"))
+b = eval(input("Enter another number"))
+operator = input("Enter a operator")
+if operator == "+":
+    print(a+b)
+elif operator == "-":
+    print(a-b)
+elif operator == "*":
+    print(a*b)
+elif operator == "/":
+    print(a/b)
