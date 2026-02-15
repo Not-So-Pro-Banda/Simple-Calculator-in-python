2205b2acbc34e908ac929815c9e3d0fdc37a7914
Muhammad Saad Bin Ishtiaq <saadbinishtiaq9@gmail.com>
2026-02-15 15:31:58 +0500

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -1,15 +1,18 @@
-a = eval(input("Enter a number: "))
-b = eval(input("Enter another number: "))
-operator = input("What do you want to do? + , - , * , / ")
-if operator == "+":
-    print(a+b)
-elif operator == "-":
-    print(a-b)
-elif operator == "*":
-    print(a*b)
-elif operator == "/":
-    print(a/b)
-elif operator == "%":
-    print(a%b)
-else:
-print("Invalid operator.")
+start = input("Enter y to start calculator or n to exit ")
+while start == "y":
+    a = eval(input("Enter a number: "))
+    b = eval(input("Enter another number: "))
+    operator = input("What do you want to do? + , - , * , / ")
+    if operator == "+":
+        print(a+b)
+    elif operator == "-":
+        print(a-b)
+    elif operator == "*":
+        print(a*b)
+    elif operator == "/":
+        print(a/b)
+    elif operator == "%":
+        print(a%b)
+    else:
+        print("Invalid operator.")
+    start = input("Enter y to start calculator or n to exit ")
